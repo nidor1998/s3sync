9a4ff0d92238e7351d7352ca144c2c8b305b667f
nidor1998 <nidor1998@gmail.com>
2024-07-20 15:24:35 +0900

    replace deprecated function.

--- src/storage/s3/upload_manager.rs
+++ src/storage/s3/upload_manager.rs
@@ -252,7 +252,10 @@ impl UploadManager {
                 self.config.content_language.clone()
             })
             .set_expires(if self.config.expires.is_none() {
-                get_object_output.expires().cloned()
+                get_object_output.expires_string().map(|expires_string| DateTime::from_str(
+                    expires_string,
+                    DateTimeFormat::HttpDate,
+                ).unwrap())
             } else {
                 Some(
                     DateTime::from_str(
@@ -751,7 +754,10 @@ impl UploadManager {
                 self.config.content_language.clone()
             })
             .set_expires(if self.config.expires.is_none() {
-                get_object_output.expires().cloned()
+                get_object_output.expires_string().map(|expires_string| DateTime::from_str(
+                    expires_string,
+                    DateTimeFormat::HttpDate,
+                ).unwrap())
             } else {
                 Some(
                     DateTime::from_str(
