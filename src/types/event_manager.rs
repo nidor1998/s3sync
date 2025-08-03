bad67a6ded44d8b3c6ad5a31e96553f095ff7dd3
nidor1998 <nidor1998@gmail.com>
2025-08-03 14:57:29 +0900

    Suppress static code analysis warnings (RS-W1070, RS-A1008, RS-W1031) across multiple modules.

--- src/types/event_manager.rs
+++ src/types/event_manager.rs
@@ -10,11 +10,9 @@ pub struct EventManager {
     pub event_flags: EventType,
 }
 
-// RS-A1008 is not applicable here as this is intentional implementation
-// skipcq: RS-A1008
 impl Default for EventManager {
-    // skipcq: RS-A1008
     fn default() -> Self {
+        // RS-A1008 is not applicable here as this is intentional implementation
         // skipcq: RS-A1008
         Self::new()
     }
