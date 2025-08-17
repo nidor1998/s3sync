87ac671c7b8386feeae8ea4b306e9d7f6082d759
nidor1998 <nidor1998@gmail.com>
2025-08-17 14:27:51 +0900

    Add test for Lua event callback execution with repeated runs and enhance event logging

--- tests/lua_callback.rs
+++ tests/lua_callback.rs
@@ -95,6 +95,81 @@ mod tests {
     }
 
     #[tokio::test]
+    async fn test_lua_event_callback_twice() {
+        TestHelper::init_dummy_tracing_subscriber();
+
+        let _semaphore = SEMAPHORE.clone().acquire_owned().await.unwrap();
+
+        let helper = TestHelper::new().await;
+        helper
+            .delete_bucket_with_cascade(&BUCKET1.to_string())
+            .await;
+
+        {
+            let target_bucket_url = format!("s3://{}", BUCKET1.to_string());
+            helper.create_bucket(&BUCKET1.to_string(), REGION).await;
+
+            let args = vec![
+                "s3sync",
+                "--target-profile",
+                "s3sync-e2e-test",
+                "--event-callback-lua-script",
+                "./test_data/script/event_callback.lua",
+                "./test_data/e2e_test/case1/",
+                &target_bucket_url,
+            ];
+
+            let config = Config::try_from(parse_from_args(args).unwrap()).unwrap();
+            let cancellation_token = create_pipeline_cancellation_token();
+            let mut pipeline = Pipeline::new(config.clone(), cancellation_token).await;
+
+            pipeline.run().await;
+
+            assert!(!pipeline.has_error());
+
+            let stats = TestHelper::get_stats_count(pipeline.get_stats_receiver());
+            assert_eq!(stats.sync_complete, 5);
+            assert_eq!(stats.e_tag_verified, 5);
+            assert_eq!(stats.sync_skip, 0);
+            assert_eq!(stats.checksum_verified, 0);
+            assert_eq!(stats.sync_warning, 0);
+        }
+
+        {
+            let target_bucket_url = format!("s3://{}", BUCKET1.to_string());
+
+            let args = vec![
+                "s3sync",
+                "--target-profile",
+                "s3sync-e2e-test",
+                "--event-callback-lua-script",
+                "./test_data/script/event_callback.lua",
+                "./test_data/e2e_test/case1/",
+                &target_bucket_url,
+            ];
+
+            let config = Config::try_from(parse_from_args(args).unwrap()).unwrap();
+            let cancellation_token = create_pipeline_cancellation_token();
+            let mut pipeline = Pipeline::new(config.clone(), cancellation_token).await;
+
+            pipeline.run().await;
+
+            assert!(!pipeline.has_error());
+
+            let stats = TestHelper::get_stats_count(pipeline.get_stats_receiver());
+            assert_eq!(stats.sync_complete, 0);
+            assert_eq!(stats.e_tag_verified, 0);
+            assert_eq!(stats.sync_skip, 5);
+            assert_eq!(stats.checksum_verified, 0);
+            assert_eq!(stats.sync_warning, 0);
+        }
+
+        helper
+            .delete_bucket_with_cascade(&BUCKET1.to_string())
+            .await;
+    }
+
+    #[tokio::test]
     async fn test_lua_event_callback_error() {
         TestHelper::init_dummy_tracing_subscriber();
 
@@ -238,6 +313,8 @@ mod tests {
                 "s3sync",
                 "--target-profile",
                 "s3sync-e2e-test",
+                "--event-callback-lua-script",
+                "./test_data/script/event_callback.lua",
                 "--filter-callback-lua-script",
                 "./test_data/script/filter_callback.lua",
                 "./test_data/e2e_test/case1/",
