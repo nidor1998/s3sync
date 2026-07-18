8963e6683d256e3a249af7425392ed3f04714e59
nidor1998 <nidor1998@gmail.com>
2026-07-18 14:06:57 +0900

    Fix: use source SSE-C parameters instead of target in `etag_diff_detector`

--- src/pipeline/diff_detector/etag_diff_detector.rs
+++ src/pipeline/diff_detector/etag_diff_detector.rs
@@ -653,9 +653,9 @@ impl ETagDiffDetector {
                     .get_object_parts(
                         key,
                         None,
-                        self.config.target_sse_c.clone(),
-                        self.config.target_sse_c_key.clone(),
-                        self.config.target_sse_c_key_md5.clone(),
+                        self.config.source_sse_c.clone(),
+                        self.config.source_sse_c_key.clone(),
+                        self.config.source_sse_c_key_md5.clone(),
                     )
                     .await
                 {
